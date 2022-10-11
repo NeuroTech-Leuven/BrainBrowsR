437467058696f8e7ea9bf830550d90ffae88b165
wout <wout.vandroogenbroeck@student.kuleuven.be>
2022-10-11 11:06:53 +0200

    added helper scripts for cleaner code

--- src/content_script.js
+++ src/content_script.js
@@ -1,10 +1,14 @@
 // Make it clear that the extension has loaded
 document.body.style.border = "10px solid lightblue";
 
-
 // wait a bit
-setTimeout(main, 300);
-// main();
+setTimeout(main, 500);
+
+function main() {
+  editPage();
+  setUp();
+}
+
 
 function setUp() {
   var currentPost = getAndProcessPost(0);
@@ -33,12 +37,6 @@ function setUp() {
   }
 }
 
-// TODO clean up the code below with imports
-
-function main() {
-  editPage();
-  setUp();
-}
 
 function editPage() {
   // hide the menubar
@@ -53,137 +51,14 @@ function editPage() {
   getAndHideElementByClassName("x1vjfegm xvb8j5");
 
   // scroll to the first post
-  getAndProcessPost(0);
+  getAndProcessPost(0); 
 
   // add the logo
-  var logo = document.createElement("img");
-  logo.setAttribute("src", makeURL("icons/logonutl.png"));
-  logo.className = "logo";
-  document.body.appendChild(logo);
+  addLogo("icons/logonutl.png")
 
   // document.body.style.backgroundColor = "lightblue";
   setBackground("lightblue");
 }
 
-/*
-Hides an html element
-*/
-function hideElement(element) {
-  element.style.display = "none";
-}
 
-/*
-Gets an element by classname and then hides it if it exists
-*/
-function getAndHideElementByClassName(className) {
-  var element = document.getElementsByClassName(className)[0];
-
-  if (element) {
-    hideElement(element);
-  } 
-  // Attempt at less hardcoded way to hide elements
-
-  // else {
-    // count ++;
-  //   console.log(className, count);
-  //   if (count > 20){
-  //     return;
-  //   }
-  //   setTimeout(getAndHideElementByClassName(className, count), 1000); // try again in 300 milliseconds
-  // }
-}
-
-
-function makeURL(img_path) {
-  // eslint-disable-next-line no-undef
-  return browser.runtime.getURL(img_path);
-}
-
-function nextFromCurrent(current) {
-  if (!current) {
-    return getPostByIndex(0);
-  }
-  let tempPosts = getPosts();
-  let ind = findByIndex(tempPosts, current);
-  let newCurrent = tempPosts[ind + 1];
-  processPost(newCurrent);
-  return newCurrent;
-}
-
-function previousFromCurrent(current) {
-  if (!current) {
-    return getPostByIndex(0);
-  }
-  let tempPosts = getPosts();
-  let ind = findByIndex(tempPosts, current);
-  let newCurrent;
-  if (ind) {
-    newCurrent = tempPosts[ind - 1];
-  } else {
-    newCurrent = current;
-  }
-  processPost(newCurrent);
-  return newCurrent;
-}
-
-function getAndProcessPost(index) {
-  var post = getPostByIndex(index);
-  processPost(post);
-  return post;
-}
-
-function processPost(post) {
-  const interactive_elements = post.getElementsByClassName("_abl-");
-  formatInteractiveElements(interactive_elements);
-  post.scrollIntoView({ behaviour: "smooth" });
-}
-
-function formatInteractiveElements(interactive_elements) {
-  hideElement(interactive_elements[0]);
-  hideElement(interactive_elements[3]);
-  hideElement(interactive_elements[4]);
-  hideElement(interactive_elements[5]);
-}
-
-function getPosts() {
-  return document.getElementsByTagName("article");
-}
-
-function getPostByIndex(index) {
-  const temp_posts = document.getElementsByTagName("article");
-  return temp_posts[index];
-}
-
-function findByIndex(posts, current) {
-  for (let i = 0; i < posts.length; i++) {
-    if (current === posts[i]) {
-      return i;
-    }
-  }
-  return 0;
-}
-
-function likePost(post) {
-  
-  post.querySelectorAll('svg[aria-label="Vind ik leuk"]').forEach(svg => svg.closest("button").click());
-
-  post.querySelectorAll('svg[aria-label="Vind ik niet meer leuk"]').forEach(svg => svg.closest("button").click());
-
-}
-
-
-async function confirmAction(color) {
-  setBackground(color)
-  await sleep(500);
-  setBackground("lightblue")
-
-}
-
-function setBackground(color) {
-  document.body.style.backgroundColor = color;
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
